3d2dca241d8c3caec153f906b2a6001d1e258551
Paul D Eden <paul.eden@byu.edu>
2016-11-13 03:27:27 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # word-of-the-day-alexa-flash-briefing-skill
 An Alexa Flash Briefing skill for telling the word of the day.  Powered by the Wordnik API.
+
+Requirements:
+* python2.7
+* jq
+* sed
---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+
+PROFILE=$1
+
+# update lambda
+gordon build
+AWS_PROFILE=$PROFILE gordon apply
+
+# update s3 site
+pipeline-scripts/cf_stack.py update alexa-word-of-the-day -f s3/s3-alexa-flash-briefing-site.yaml -a $PROFILE
+BUCKET=`aws cloudformation describe-stacks --stack-name alexa-word-of-the-day --profile $PROFILE | jq .Stacks[0].Outputs[0].OutputValue | sed -e "s/\"//g"`
+cd s3/contents
+aws s3 sync . s3://$BUCKET/ --profile $PROFILE --acl public-read
